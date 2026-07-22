c9a913741e9113679bc2962288729b0ac7e4bb83
sergeigorbunov2011-gif <sergeigorbunov2011@gmail.com>
2026-07-22 23:54:27 +0600

    Update DarkChat.java

--- src/main/java/me/darkmine/darkchat/DarkChat.java
+++ src/main/java/me/darkmine/darkchat/DarkChat.java
@@ -1,1 +1,38 @@
+package me.darkmine.darkchat;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DarkChat extends JavaPlugin {
+
+    private static DarkChat instance;
+
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        saveDefaultConfig();
+
+        Bukkit.getConsoleSender().sendMessage("§8==============================");
+        Bukkit.getConsoleSender().sendMessage("§aDarkChat §7успешно загружен!");
+        Bukkit.getConsoleSender().sendMessage("§7Версия: §f1.0");
+        Bukkit.getConsoleSender().sendMessage("§7Автор: §bSergey");
+        Bukkit.getConsoleSender().sendMessage("§8==============================");
+
+        // Здесь позже зарегистрируем команды и события
+
+    }
+
+    @Override
+    public void onDisable() {
+
+        Bukkit.getConsoleSender().sendMessage("§cDarkChat выключен.");
+
+    }
+
+    public static DarkChat getInstance() {
+        return instance;
+    }
+
+}
